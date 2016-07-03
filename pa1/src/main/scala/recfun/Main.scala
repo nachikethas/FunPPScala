a3685de5bc1d2eede19e1e278caefe884ed76de3
Nachikethas <nachikethas@users.noreply.github.com>
2016-07-03 20:52:34 +0530

    Implements recursion exercises

---
+++ pa1/src/main/scala/recfun/Main.scala
@@ -0,0 +1,49 @@
+package recfun
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0 || c == r || r == 0) 1 else
+    pascal(c-1, r-1) + pascal(c, r-1)
+  }
+  
+  /**
+   * Exercise 2
+   */
+  def balance(chars: List[Char]): Boolean = {
+    def balanceIter(acc: Int, chars: List[Char]): Boolean = {
+      if (acc < 0 || (chars.isEmpty && acc != 0)) false else
+      if (chars.isEmpty && acc == 0) true else
+      if (chars.head == '(') balanceIter(acc + 1, chars.tail) else
+      if (chars.head == ')') balanceIter(acc - 1, chars.tail) else
+      balanceIter(acc, chars.tail)
+    }
+
+    balanceIter(0, chars)
+  }
+  
+  /**
+   * Exercise 3
+   */
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (coins.isEmpty || money == 0) 0 else
+    if (coins.tail.isEmpty && money % coins.head != 0) 0 else
+    if (coins.tail.isEmpty && money % coins.head == 0) 1 else
+    if (coins.head > money) countChange(money, coins.tail) else
+    if (coins.head == money) countChange(money, coins.tail) + 1 else
+    countChange(money, coins.tail) +
+    countChange(money-coins.head, coins)
+
+  }
+  }
